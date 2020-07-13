7b18fc78cbbc4bab6198f706ab5b0b1124bfa6f3
Manasvi Saxena <msaxena2@illinois.edu>
2020-07-13 15:57:53 -0500

    build,gen-monitors:Add scripts for building+generating sources

---
+++ gen-monitor.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+progress () { echo "====== $@" ; }
+
+# Executables
+# -----------
+
+repo_dir="$(cd "$(dirname "$0")"; pwd)"
+specs_dir="$repo_dir/specs"
+rvmonitor_bin="$repo_dir/ext/rv-monitor/target/release/rv-monitor/bin"
+javamop_bin="$repo_dir/ext/javamop/target/release/javamop/javamop/bin"
+gen_src_dir="$repo_dir/target/generated-sources"
+
+progress "generate aspectJ and rvm files"
+
+(   ( mkdir -p "$gen_src_dir/aspectJ" && mkdir -p "$gen_src_dir/java" && \
+      cd "$specs_dir" && \
+      "$javamop_bin/javamop" -debug -d "$gen_src_dir/aspectJ" -merge *.mop && \
+      "$rvmonitor_bin/rv-monitor" -merge -d "$gen_src_dir/java" *.rvm && ) \
+ || ( rm -f "$gen_src_dir/aspectJ" ; rm -f "$gen_src_dir/java/*.java" ))
